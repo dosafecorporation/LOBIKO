796003c382fa0ae1a14eac459683e946d5d20660
isaacket97 <isaacket97@gmail.com>
2025-07-10 21:05:09 +0100

    first commit

--- lobiko/admin.py
+++ lobiko/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
-from .models import CompteWhatsApp, Patient, Assureur, Assurance, Medecin, SessionDiscussion, Message
+from .models import Patient, Assureur, Assurance, SessionDiscussion, Medecin, Message
 
 # Register your models here.
-admin.site.register(CompteWhatsApp)
 admin.site.register(Patient)
 admin.site.register(Assureur)
 admin.site.register(Assurance)
-admin.site.register(Medecin)
 admin.site.register(SessionDiscussion)
+admin.site.register(Medecin)
 admin.site.register(Message)
