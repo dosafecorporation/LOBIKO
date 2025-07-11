12b334fedf14025f59aafe17f034728d2bcc1646
isaacket97 <isaacket97@gmail.com>
2025-07-11 15:35:40 +0100

    medecin update

--- lobikohealth/urls.py
+++ lobikohealth/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('bot/', include('bot.urls')),
-    path('medecin/', include('medecin.urls')),
+    path('medecins/', include('medecins.urls')),
 ]
